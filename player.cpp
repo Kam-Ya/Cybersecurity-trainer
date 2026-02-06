4a98fcedb68175d3c2cd44b98db6229e54ae965c
Kam-Ya <kamranyaghoubian@gmail.com>
2026-02-06 13:37:01 -0500

    added a basic player class

---
+++ player.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "player.hpp"
+
+player::getPoints() {
+	return points;
+}
+
+player::setPoints(int a) {
+	this.points = a;
+}
+
+player::getMult() {
+	return mult;
+}
+
+player::setPoints(int a) {
+	this.mult = a;
+}
---
+++ player.hpp
@@ -0,0 +1,18 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+class player {
+
+	private:
+		int points;
+		int mult;
+
+	public:
+		int getPoints();
+		int setPoints(int a);
+		int getMult();
+		int getMult(int a);
+
+}
+
+#endif
