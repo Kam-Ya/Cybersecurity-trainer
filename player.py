df593d0576e3af1368ef5c896e46f2465aa761f0
Kam-Ya <kamranyaghoubian@gmail.com>
2026-03-03 15:13:46 -0500

    created memory for previous passwords

--- player.py
+++ player.py
@@ -1,10 +1,18 @@
 class Player:
     __points
     __mult
+    __prevPass
 
     def __init___(self, points, mult):
         self.__points = 0
         self.__mult = 0.75
+        self.__mult = []
+
+    def setPrev(self, wrd):
+        self.__prevPass.append(wrd)
+
+    def getPrev(self):
+        return self.__prevPass[-1]
 
     def getPoints(self):
         return self.__points;
